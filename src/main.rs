e1a3731082edde03df8754cd3044d5952fceb3d6
root <root@PCV2.localdomain>
2026-04-04 19:34:48 -0600

    renamed crate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::f64::consts::PI;
 
-use complex_bspline::bsplines::dump::dump_basis;
-use complex_bspline::bsplines::{
+use bspline_basis::bsplines::dump::dump_basis;
+use bspline_basis::bsplines::{
     basis::BSplineBasis,
     complex::{ComplexBSplineBasis, ComplexBSplineBasisConfig},
     real::BSplineBasisConfig,
 };
-use complex_bspline::knots::complex::EcsConfig;
+use bspline_basis::knots::complex::EcsConfig;
 fn main() {
     let ecs_config = EcsConfig {
         r0: 5.0,
